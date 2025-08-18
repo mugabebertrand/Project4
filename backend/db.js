4b574a7cea8f4011d4ed2f7e3a1b46f8ccee4fa5
mugabebertrand <mugabebertrand@gmail.com>
2025-08-18 15:17:08 -0500

    backend

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// create a connection pool
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'mugabe2023!',
+    database: 'Project4',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+const promisePool = pool.promise();
+
+module.exports = promisePool;
