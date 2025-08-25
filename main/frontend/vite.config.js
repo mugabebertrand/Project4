8106241cf62b6f841f9ba4b55a8f659ad8a5a2f5
mugabebertrand <mugabebertrand@gmail.com>
2025-08-25 00:12:43 -0500

    check vite.config

--- main/frontend/vite.config.js
+++ main/frontend/vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT for GitHub Pages
+  base: '/Project4/',
+
+  // Dev server only (local development)
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // backend server
+        target: 'http://localhost:3000', // your Node/Express backend in dev
         changeOrigin: true,
         secure: false
       }
